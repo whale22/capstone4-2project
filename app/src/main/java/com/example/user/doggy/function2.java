79db20e74a9fefe4835220b0851b14a9311363b7
jaeshin-e <jsqs96@gmail.com>
2018-10-27 21:48:50 +0900

    진행사항

---
+++ app/src/main/java/com/example/user/doggy/function2.java
@@ -0,0 +1,27 @@
+package com.example.user.doggy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class function2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_function2);
+
+        Button btn = (Button)findViewById(R.id.back);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/user/doggy/intro.java
@@ -0,0 +1,31 @@
+package com.example.user.doggy;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();//액션바 숨기기
+
+        //3초 후 인트로 액티비티 제거
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(intro.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, 8000);
+    }
+}
